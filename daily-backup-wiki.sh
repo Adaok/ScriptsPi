86e70f97f5638229d36e33a1d5f824e8152fca33
Marco Loiodice <mloiodice13@gmail.com>
2017-01-02 21:32:06 +0100

    change mount folder and script

--- daily-backup-wiki.sh
+++ daily-backup-wiki.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir "$(date +"%Y-%m-%d")"
-zip -r /backup/"$(date +"%Y-%m-%d")"/configuration.zip /etc/dokuwiki
-zip -r /backup/"$(date +"%Y-%m-%d")"/acces.zip /var/lib/dokuwiki/acl
-zip -r /backup/"$(date +"%Y-%m-%d")"/data.zip /var/lib/dokuwiki/data
+mkdir /mnt/backup/"$(date +"%Y-%m-%d")"
+zip -r /mnt/backup/"$(date +"%Y-%m-%d")"/configuration.zip /etc/dokuwiki
+zip -r /mnt/backup/"$(date +"%Y-%m-%d")"/acces.zip /var/lib/dokuwiki/acl
+zip -r /mnt/backup/"$(date +"%Y-%m-%d")"/data.zip /var/lib/dokuwiki/data
